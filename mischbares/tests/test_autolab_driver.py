d28ed5d27abe88732e2c0385d0e58b4a11d1a55d
Fuzhan R <fuzhanrahmanian@gmail.com>
2022-11-02 12:05:25 +0100

    add test file for potential and current

--- mischbares/tests/test_autolab_driver.py
+++ mischbares/tests/test_autolab_driver.py
@@ -1,6 +1,4 @@
 """ Test file for the autolab driver """
-import os
-import pytest
 
 from mischbares.config.main_config_2 import config
 from mischbares.driver.autolab_driver import Autolab
@@ -8,5 +6,24 @@ from mischbares.driver.autolab_driver import Autolab
 AUTOLAB = Autolab(config["autolabDriver"])
 
 def test_load_procedure():
+    """Test loading a procedure
+    """
     AUTOLAB.load_procedure("ocp")
     assert AUTOLAB.proc.FileName[-21:-4] == "OCP_record_signal"
+
+
+def test_potential():
+    """Test setting a potential
+    """
+    applied_potential = AUTOLAB.potential()
+    assert round(applied_potential, 2) == 0.0
+
+
+def test_current():
+    """Test setting a current
+    """
+    applied_current = AUTOLAB.current()
+    assert round(applied_current, 2) == 0.0
+
+
+# def test_

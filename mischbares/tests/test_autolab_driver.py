49d6ae2a7ae509e7d42dbf1d0824fca5ad078af1
Fuzhan R <fuzhanrahmanian@gmail.com>
2022-11-16 22:08:18 +0100

    additional testing

--- mischbares/tests/test_autolab_driver.py
+++ mischbares/tests/test_autolab_driver.py
@@ -10,7 +10,7 @@ def test_load_procedure():
     """Test loading a procedure
     """
     AUTOLAB.load_procedure("ocp")
-    assert AUTOLAB.proc.FileName[-21:-4] == "OCP_record_signal"
+    assert AUTOLAB.proc.FileName[-18:-4] == "OCP_MISCHBARES"
 
 
 def test_potential():
@@ -45,7 +45,8 @@ def test_set_setpoints():
     """Test setting the setpoints
     """
     AUTOLAB.load_procedure("ocp")
-    AUTOLAB.set_setpoints({'FHLevel': {'Duration': 20}})
+    AUTOLAB.set_setpoints(procedure = "ocp", setpoints = {'recordsignal': {'Duration (s)': 20}},\
+                            current_range = "1uA")
     assert AUTOLAB.proc.Commands["FHLevel"].CommandParameters["Duration"].Value == 20
 
 
@@ -62,10 +63,57 @@ def test_measure():
     """Test measuring
     """
     AUTOLAB.load_procedure("ocp")
-    AUTOLAB.set_setpoints({'recordsignal': {'Duration (s)': 10}})
+    AUTOLAB.set_setpoints(procedure = "ocp", setpoints = {'recordsignal': {'Duration (s)': 10}},\
+                            current_range = "1uA")
     AUTOLAB.proc.Measure()
     AUTOLAB.proc.SaveAs(os.path.join(os.getcwd(), "results", "test.nox"))
     assert len([str(n) for n in AUTOLAB.proc.Commands['recordsignal'].Signals.Names]) == 10
+
+
+def test_ocp_on_the_fly():
+    """Test measuring open circuit potential on the fly
+    """
+    _, ocp_potential = AUTOLAB.get_ocp_on_the_fly()
+    assert round(ocp_potential, 2) == 0.0
+
+# from here can be just run by debugging
+
+def test_ca_at_ocp():
+    """ Test performing ca measurement at ocp potential
+    """
+    data = AUTOLAB.perform_measurement(procedure = "ca",\
+            setpoints = {'applypotential': {'Setpoint value': 0.7},\
+                'recordsignal': {'Duration (s)': 5, 'Interval time (s)': 0.5}},\
+            save_dir= "mischbares/tests",
+            plot_type ='tCV', parse_instruction = ['recordsignal'], \
+            current_range= "100uA", measure_at_ocp = True)
+    assert len(data['recordsignal'].keys()) == 11
+    assert round(data['recordsignal']['WE(1).Potential'][-1], 1) == 0.7
+
+
+def test_cp_at_ocp():
+    """ Test performing cp measurement at ocp potential
+    """
+    data = AUTOLAB.perform_measurement(procedure = "cp",\
+            setpoints = {'applycurrent': {'Setpoint value': 0.00001},\
+                'recordsignal': {'Duration (s)': 10, 'Interval time (s)': 0.5}},\
+            save_dir= "mischbares/tests",\
+            plot_type ='tCV', parse_instruction = ['recordsignal'], \
+            current_range = "100uA", measure_at_ocp = True)
+    assert len(data['recordsignal'].keys()) == 11
+    assert round(data["recordsignal"]['WE(1).Current'][-1], 1) == 0.0
+
+
+def test_eis_at_ocp():
+    """ Test performing eis measurement at ocp potential
+    """
+    data = AUTOLAB.perform_measurement(procedure = "eis", setpoints = None, \
+            save_dir= "mischbares/tests",
+            plot_type ='impedance', \
+            parse_instruction = ["FIAMeasPotentiostatic", "FIAMeasurement"], \
+            current_range= "1mA", measure_at_ocp = True)
+    assert len(data.keys()) ==2
+    assert round(data["FIAMeasurement"]['Potential (DC)'][0], 1) == 0.0
 
 
 def test_disconnect():

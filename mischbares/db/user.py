7ac8c4506e42df42972d168e559971f58c8da163
Fuzhan R <fuzhanrahmanian@gmail.com>
2022-12-05 09:28:22 +0100

    connect to user table in postgres

--- mischbares/db/user.py
+++ mischbares/db/user.py
@@ -1,22 +1,19 @@
-import psycopg2
+""" Class for handling the User table in the database. """
+from bcrypt import hashpw, gensalt, checkpw
 
-from bcrypt import hashpw, gensalt
-
+from mischbares.db.database import Database
 from mischbares.logger import logger
 
 log = logger.get_logger("db_users")
 
-class Users():
-    """class for handling user data. This class inherits from the Database class"""
-    def __init__(self, connection, cursor):
+class Users(Database):
+    """class for handling user data."""
+    def __init__(self):
         """Initialize the User class
-
-        Args:
-            connection (psycopg2.connection): The connection to the database
-            cursor (psycopg2.cursor): The cursor to execute SQL statements
         """
-        self.connection = connection
-        self.cursor = cursor
+        super().__init__()
+        self.user_id = None
+
 
     def get_user(self, username):
         """get a user from the database
@@ -27,10 +24,8 @@ class Users():
             user (tuple): A tuple containing the user's data
         """
         sql = "SELECT * FROM users WHERE username = %s"
-        self.cursor.execute(sql, (username,))
-        result = self.cursor.fetchall()
-        cols = [desc[0] for desc in self.cursor.description]
-        return dict(zip(cols, result[0]))
+        return self.execute(sql, (username,))
+
 
     def register_user(self, username, first_name, last_name, email, password):
         """register a user in the database
@@ -47,15 +42,14 @@ class Users():
         if username_exists:
             log.info("User already exists")
             return False
-        self.cursor.execute("INSERT INTO users (username, first_name, last_name, email, password) \
-                            VALUES (%s, %s, %s, %s, %s)", (username, first_name, last_name, email, hashed_password))
-        try:
-            self.connection.commit()
+
+        commit_status = self.commit("INSERT INTO users (user_id, first_name, last_name, email, password, username)\
+            VALUES (nextval('user_user_id_seq'::regclass), %s, %s, %s, %s, %s)", \
+            (first_name, last_name, email, hashed_password.decode('utf-8'), username))
+        if commit_status:
             log.info(f"User {username} registered.")
-            return True
-        except Exception as e:
-            log.error(e)
-            return False
+        return commit_status
+
 
     def login_user(self, username, password):
         """login a user
@@ -67,25 +61,28 @@ class Users():
             bool: True if the user was logged in, False if not
         """
         sql = "SELECT * FROM users WHERE username = %s"
-        result = self.cursor.execute(sql, (username,))
+        result = self.execute(sql, (username,))
         if result:
-            if hashpw(password.encode('utf-8'), result[0][2]) == result[0][2]:
+            if checkpw(password.encode('utf-8'), result['password'].encode('utf-8')):
                 log.info(f"Password correct. User {username} logged in.")
+                self.user_id = result['user_id']
                 return True
-            else:
-                log.info("Password incorrect.")
-                return False
-        else:
-            log.info(f"User {username} does not exist.")
+            log.info("Password incorrect.")
             return False
+        log.info(f"User {username} does not exist.")
+        return False
+
 
     def delete_user(self, username):
         """Delete a user from the database
 
         Args:
             username (str): The username of the user
+        Returns:
+            bool: True if the user was deleted, False if not
         """
         sql = "DELETE FROM users WHERE username = %s"
-        self.cursor.execute(sql, (username,))
-        self.connection.commit()
-        log.info(f"User {username} deleted.")
+        commit_status = self.commit(sql, (username,))
+        if commit_status:
+            log.info(f"User {username} deleted.")
+        return commit_status

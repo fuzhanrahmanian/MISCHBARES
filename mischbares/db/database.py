5331b3cc2a545fb200659ac0c3a53712808290bc
Fuzhan R <fuzhanrahmanian@gmail.com>
2022-12-05 09:27:42 +0100

    postgres database class

--- mischbares/db/database.py
+++ mischbares/db/database.py
@@ -1,3 +1,5 @@
+"""Class for database objects and connections."""
+import configparser
 import psycopg2
 
 from mischbares.logger import logger
@@ -5,21 +7,27 @@ from mischbares.logger import logger
 log = logger.get_logger("db_database")
 
 class Database:
-    def __init__(self, host, port, user, password, database):
-        self.host = host
-        self.port = port
-        self.user = user
-        self.password = password
-        self.database = database
+    """ Class for database objects and connections. """
+    def __init__(self):
+        config  = configparser.ConfigParser()
+        config.read("mischbares/db/config.ini")
+        self.host = config["database"]["host"]
+        self.port = config["database"]["port"]
+        self.user = config["database"]["user"]
+        self.password = config["database"]["password"]
+        self.database = config["database"]["database"]
         self.connection = None
         self.cursor = None
+        self.connect()
+
+
+    def __del__(self):
+        """Close the connection to the database"""
+        self.close()
+
 
     def connect(self):
         """Connect to the database
-
-        Returns:
-            connection (psycopg2.connection): The connection to the database
-            cursor (psycopg2.cursor): The cursor to execute SQL statements
         """
         self.connection = psycopg2.connect(
             host=self.host,
@@ -31,17 +39,40 @@ class Database:
         try:
             self.cursor = self.connection.cursor()
             log.info("Connected to database")
-            return self.connection, self.cursor
+
         except psycopg2.Error as e:
             log.error(f"Error while connecting to database: {e}")
-            return None
+
 
     def close(self):
         """Close the connection to the database"""
         self.connection.close()
 
+
     def execute(self, sql, params=None):
         """Execute a SQL statement"""
         self.cursor.execute(sql, params)
         result = self.cursor.fetchall()
-        return result
+        if result:
+            cols = [desc[0] for desc in self.cursor.description]
+            return dict(zip(cols, result[0]))
+        return None
+
+
+    def commit(self, sql, params=None):
+        """Commit a SQL statement
+        Args:
+            sql (str): The SQL statement to execute
+            params (tuple): The parameters for the SQL statement
+        Returns:
+            Bool: True if the commit was successful, False otherwise
+        """
+        try:
+            self.cursor.execute(sql, params)
+            self.connection.commit()
+            log.info("Changes committed to database")
+            return True
+
+        except psycopg2.Error as e:
+            log.error(f"Error while executing SQL statement: {e}")
+            return False

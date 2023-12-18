73b39ef9172d335e5964e1aa1ff5e3385a0fc359
Fuzhan R <fuzhanrahmanian@gmail.com>
2023-12-18 21:45:32 +0100

    add motor and camera to main

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 """Main module."""
 import subprocess
 import os
-from datetime import datetime
 import requests
 import time
 import shutil
 import json
-import argparse
 from multiprocessing import Process
 
 # postgres
-import psycopg2
+
 from mischbares.logger import logger
 from mischbares.action import autolab_action
 from mischbares.server import autolab_server
 from mischbares.orchestrator import orchestrator
 from mischbares.config.main_config import config
-from mischbares.procedures.autolab_procedures import AutolabProcedures
 
-from mischbares.db.database import Database
+from mischbares.utils.utils import send_to_telegram
+
 from mischbares.db.user import Users
-from mischbares.db.experiment import Experiments
-from mischbares.db.measurement import Measurements
-from mischbares.db.procedure import Procedure
+
+from mischbares.server import lang_server
+from mischbares.action import lang_action
+
+from mischbares.action import hamilton_action
+from mischbares.server import hamilton_server
+
 
 log = logger.setup_applevel_logger(file_name="mischbares.log")
 
@@ -30,6 +32,26 @@ host_url = config['servers']['autolab']['host']
 port_action = config['servers']['autolab']['port']
 port_server = config['servers']['autolabDriver']['port']
 port_orchestrator = config['servers']['orchestrator']['port']
+
+
+def run_lang_action():
+    """Start the Autolab server."""
+    lang_action.main()
+
+def run_lang_server():
+    """Start the Autolab server."""
+    lang_server.main()
+
+
+def run_action_hamilton():
+    """Start the Autolab server."""
+    hamilton_action.main()
+
+
+def run_server_hamilton():
+    """Start the Autolab server."""
+    hamilton_server.main()
+
 
 def run_action():
     """Start the Autolab server."""
@@ -52,7 +74,11 @@ def start_bokeh_visualizer():
 
 def banana_instance():
     """Start the server and action in a separate processes."""
-    processes = [Process(target=run_server),
+    processes = [Process(target=run_server_hamilton),
+                 Process(target=run_action_hamilton),
+                 Process(target=run_lang_server),
+                 Process(target=run_lang_action),
+                 Process(target=run_server),
                  Process(target=run_action),
                  Process(target=run_orchestrator)]
 
@@ -62,7 +88,7 @@ def banana_instance():
             print("Waiting for processess to start...")
         except RuntimeError as e:
             print("Error starting server: ", e)
-    time.sleep(15)
+    time.sleep(40)
     yield processes
     for proc in processes:
         proc.kill()
@@ -74,7 +100,7 @@ def start_orchestrator_experimentation():
     """ Intantiate the orchestrator scheduler. """
     # Assuming the start fucniotns works
     sequence = dict(soe=['orchestrator/start'],
-                  params={'start': {'collectionkey': "db_test"}}, meta={})
+                  params={'start': {'collectionkey': "qc_drop_test"}}, meta={})
     params = dict(experiment=json.dumps(sequence),thread=0)
     requests.post(f"http://{host_url}:{port_orchestrator}/orchestrator/addExperiment",
                             params=params, timeout=None).json()
@@ -87,7 +113,7 @@ def end_orchestrator_experimentation():
     params = dict(experiment=json.dumps(sequence),thread=0)
     requests.post(f"http://{host_url}:{port_orchestrator}/orchestrator/addExperiment",
                             params=params, timeout=None).json()
-
+    send_to_telegram(message=f"Experiment finished: \n Closing experiment", message_type="info")
 
 def main():
     """Main function."""
@@ -111,24 +137,54 @@ def main():
     # start the orchestrator
     start_orchestrator_experimentation()
     curr = [0.0001, 0.0005]
+
+    amount_of_pump = [0, 600]
+    
+
     for i in range(1, 3):
+
+        #requests.get(f"http://{lang_host_url}:{lang_port_action}/lang/moveWaste", timeout=None)
+        soe = ['lang/moveWaste_0', 'hamilton/pumpR_0']
+        params = {'moveWaste_0': {'x_pos': 0, 'y_pos':0, 'z_pos':0},
+                  'pumpR_0':{'volume': amount_of_pump[i-1]}}
+        sequence = dict(soe=soe,params=params,meta={})
+        parameters = dict(experiment=json.dumps(sequence),thread=0)
         #create an experiment object and get the experiment id
         #TODO create a procedure object with the measurement id and the experiment id and give it to the function
-        _, _, sequence = AutolabProcedures(measurement_num=0, material="LFP", user_id=2, number_of_electrons=2,
-                                        electrode_area=6.2, concentration_of_active_material=6.2,
-                                        mass_of_active_material=6.2).cp_measurement(apply_current=curr[i-1])
+        # _, _, sequence = AutolabProcedures(measurement_num=0, material="LFP", user_id=2, number_of_electrons=2,
+        #                                 electrode_area=6.2, concentration_of_active_material=6.2,
+        #                                 mass_of_active_material=6.2).cp_measurement(apply_current=curr[i-1])
         # _, _, sequence = AutolabProcedures(measurement_num=i,
         #                        save_dir=r"C:\Users\LaborRatte23-3\Documents\repositories\test_data_mischbares",
         #                                 number_of_electrons=2, electrode_area=6.2, concentration_of_active_material=6.2,
         #                                 mass_of_active_material=6.2,
         #                        user_id=users.user_id, material="LFP").ocp_measurement()
-        params = dict(experiment=json.dumps(sequence),thread=0)
-        requests.post(f"http://{host_url}:{port_orchestrator}/orchestrator/addExperiment", params=params, timeout=None).json()
-        
-        # Wait for input from the user to continue
-        input("Press Enter to finish...")
+        # params = dict(experiment=json.dumps(sequence),thread=0)
+        requests.post(f"http://{host_url}:{port_orchestrator}/orchestrator/addExperiment", params=parameters,
+                      timeout=None).json()
+        #input("Press Enter to continue...")
 
-    end_orchestrator_experimentation()
+
+    # Run a while loop for 200 seconds
+    start_time = time.time()
+    time_in = time.time()
+    time_out = 200*60
+
+    while True:
+        # check the orchestrator is on pause every 10 seconds
+
+        response = requests.post(f"http://{host_url}:{port_orchestrator}/orchestrator/getStatus", timeout=None).json()
+
+        if response['0']['status'] == "paused":
+            end_orchestrator_experimentation()
+            break
+
+        # check if the time is out
+        if time.time() - time_in > time_out:
+            end_orchestrator_experimentation()
+            break
+        # wait for 10 seconds
+        time.sleep(10)
 
 def is_server_ready(url):
     try:
@@ -137,7 +193,9 @@ def is_server_ready(url):
     except requests.RequestException:
         return False
 
+
 def wait_for_servers_to_be_ready():
+    # TODO : check if others need it or not
     server_urls = [f"http://{host_url}:{port_server}/health",
                    f"http://{host_url}:{port_action}/health",
                    f"http://{host_url}:{port_orchestrator}/health"]
@@ -148,9 +206,14 @@ def wait_for_servers_to_be_ready():
         print("Waiting for servers to be ready...")
         time.sleep(1)
 
+
 # main function
 if __name__ == "__main__":
-    processes = [Process(target=run_server),
+    processes = [Process(target=run_server_hamilton),
+                 Process(target=run_action_hamilton),
+                 Process(target=run_lang_server),
+                 Process(target=run_lang_action),
+                 Process(target=run_server),
                  Process(target=run_action),
                  Process(target=run_orchestrator)]
 
@@ -162,6 +225,10 @@ if __name__ == "__main__":
     visualizer_process.start()
     print("Starting visualizer...")
     time.sleep(15)
+    input("Press Enter to start the experiment...")
     main()
     for proc in processes:
         proc.kill()
+
+
+

8ddd92d428f7b3518656784fa33269ecba0ae841
Fuzhan R <fuzhanrahmanian@gmail.com>
2023-12-20 00:35:18 +0100

    Adjusted main config

--- mischbares/config/main_config.py
+++ mischbares/config/main_config.py
@@ -51,6 +51,6 @@ config['lang'] = dict(langDriver= dict(velocity_x=5, velocity_y=5, velocity_z=5,
 
 config["QC"] = dict(waste_camera=dict(camera_num=0,offset_x=40, offset_y=120, delay=10, timeout=30, kill_timeout=60),
                     telegram=dict(api_token="", chat_id=""),
-                    lang=dict(potential_threshold=0.1, max_critical_steps=3, timeout=60*1),)
+                    lang=dict(potential_threshold=0.1, max_critical_steps=5, timeout=60*1),)
 # TEST POISTION FOR THE LANG QC: [45.7, 27.2, 10] -> [45.7, 27.2, 13] -> [45.7, 27.2, 15.75]
 config.update(autolab_config)
